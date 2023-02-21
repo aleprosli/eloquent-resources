035bfb253d455d0b3574e81a2bddbe4395ce6351
aliff <aliff.rosli96@gmail.com>
2023-02-22 01:10:02 +0800

    add: pagination

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -11,6 +11,6 @@ class UserController extends Controller
 {
     public function index()
     {
-        return UserResource::collection(User::all());
+        return UserResource::collection(User::paginate(5));
     }
 }
