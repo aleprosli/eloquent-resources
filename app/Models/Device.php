d39c62763f2d735beb94c77823a4acbbfb63f24a
aliff <aliff.rosli96@gmail.com>
2023-02-22 00:51:00 +0800

    add: relation user hasmany devices

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -11,4 +11,9 @@ class Device extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
